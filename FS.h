89bb9e99da20a2c27d479a8ed145f14c708e06df
zorkiy <admin@zorkiy.pw>
2024-03-26 17:26:40 +0500

    убрал лишние virtual

--- FS.h
+++ FS.h
@@ -7,8 +7,8 @@ protected:
 	unsigned int clusterSize = {0};
 	virtual bool ReadClusterSize();
 public:
-	virtual bool Init(LPCWSTR);
-	virtual bool ReadCluster(unsigned int, BYTE*);
-	virtual unsigned int ClusterSize() { return clusterSize; };
+	bool Init(LPCWSTR);
+	bool ReadCluster(unsigned int, BYTE*);
+	unsigned int ClusterSize() { return clusterSize; };
 	virtual ~FS();
 };
